67b41faccdc13299b3e9da5b9f872469fb33905a
Omar Nabi <onabi20@gmail.com>
2020-04-02 05:16:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if (age < 10)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
